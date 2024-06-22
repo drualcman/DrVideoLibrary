fdf371b4094ec895f77e2c0334dedd5f571e1b55
Sergi Ortiz Gomez <sergi.ortiz.gomez@gluonconsulting.com>
2024-06-22 13:24:47 +0800

    Add section key to follow good practice about Options pathern

--- src/DrVideoLibrary.Backend.ApplicationBusinessRules/Options/SearchMovieEnglishOption.cs
+++ src/DrVideoLibrary.Backend.ApplicationBusinessRules/Options/SearchMovieEnglishOption.cs
@@ -1,6 +1,7 @@
 ﻿namespace DrVideoLibrary.Backend.ApplicationBusinessRules.Options;
 public class SearchMovieEnglishOption
 {
+    public const string SectionKey = nameof(SearchMovieEnglishOption);
     public string SearchEnglishUrl { get; set; }
     public string SearchEnglishApiKey { get; set; }
     public SearchMovieLang Language => SearchMovieLang.EN;
--- src/DrVideoLibrary.Backend.ApplicationBusinessRules/Options/SearchMovieSpanishOption.cs
+++ src/DrVideoLibrary.Backend.ApplicationBusinessRules/Options/SearchMovieSpanishOption.cs
@@ -1,6 +1,7 @@
 ﻿namespace DrVideoLibrary.Backend.ApplicationBusinessRules.Options;
 public class SearchMovieSpanishOption
 {
+    public const string SectionKey = nameof(SearchMovieSpanishOption);
     public string SearchSpanishUrl { get; set; }
     public string SearchSpanishApiKey { get; set; }
     public SearchMovieLang Language => SearchMovieLang.ES;
