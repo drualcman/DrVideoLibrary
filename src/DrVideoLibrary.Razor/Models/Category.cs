38725d5f26daaf6e3894fca081114681eb5189be
Sergi Ortiz Gomez <sergi.ortiz.gomez@gluonconsulting.com>
2024-06-23 12:52:27 +0800

    Add category filter

---
+++ src/DrVideoLibrary.Razor/Models/Category.cs
@@ -0,0 +1,6 @@
+﻿namespace DrVideoLibrary.Razor.Models;
+public class Category
+{
+    public int Count { get; set; }
+    public string Name { get; set; }
+}
--- src/DrVideoLibrary.Razor/ViewModels/IndexViewModel.cs
+++ src/DrVideoLibrary.Razor/ViewModels/IndexViewModel.cs
@@ -10,12 +10,22 @@ public class IndexViewModel : PaginatorViewModel<ListCard>
     }
     public int TotalMovies { get; private set; }
     public bool IsReady { get; private set; }
+    public IEnumerable<Category> Categories { get; private set; }
+    public string SelectedCategory {  get; private set; } 
 
     public async Task GetList()
     {
         IEnumerable<ListCard> movies = await CacheService.GetList();
         TotalMovies = movies.Count();
         InitializePaginator(movies.ToList());
+        Categories = movies
+            .SelectMany(x => x.Categories)
+            .GroupBy(category => category)
+            .Select(grup => new Category
+            {
+                Name = grup.Key,
+                Count = grup.Count()
+            });
         IsReady = true;
     }
 
@@ -30,6 +40,22 @@ public class IndexViewModel : PaginatorViewModel<ListCard>
         {
             IEnumerable<ListCard> movies = await CacheService.GetList();
             movies = movies.Where(x => x.Title.Contains(movie, StringComparison.InvariantCultureIgnoreCase));
+            TotalMovies = movies.Count();
+            InitializePaginator(movies.ToList());
+        }
+        else await GetList();
+    }
+
+    public bool IsSelectedCategory(string category) =>
+        category.Equals(SelectedCategory, StringComparison.InvariantCultureIgnoreCase);
+
+    public async Task FilterbyCategory(string category)
+    {
+        SelectedCategory = category;
+        if (!string.IsNullOrEmpty(category))
+        {
+            IEnumerable<ListCard> movies = await CacheService.GetList();
+            movies = movies.Where(x => x.Categories.Any(c => c.Equals(category, StringComparison.InvariantCultureIgnoreCase)));
             TotalMovies = movies.Count();
             InitializePaginator(movies.ToList());
         }
