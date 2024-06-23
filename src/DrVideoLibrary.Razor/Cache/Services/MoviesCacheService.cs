65b0635a95327e85e63930912a41a347160e772e
Sergi Ortiz Gomez <sergi.ortiz.gomez@gluonconsulting.com>
2024-06-23 12:25:58 +0800

    Improves how cache update the items. Minimum once a day.

--- src/DrVideoLibrary.Razor/Cache/Services/MoviesCacheService.cs
+++ src/DrVideoLibrary.Razor/Cache/Services/MoviesCacheService.cs
@@ -6,32 +6,68 @@ public class MoviesCacheService(MoviesContext CacheContext, ApiClient Client, IJ
         IEnumerable<ListCard> movies = [];
         List<MovieCardModel> cached = await CacheContext.Movies.SelectAsync();
         if (await GetShouldUpdate())
+        {
+            UpdateCache();
             cached = null;
+        }
         if (cached == null || !cached.Any())
         {
-            movies = new List<ListCard>(await Client.GetMovies());
-            await CacheContext.Movies.CleanAsync();            
-            _ = CacheContext.Movies.AddAsync(movies.Select(MovieCardModel.FromListCard).ToList());
+            movies = await Client.GetMovies();
         }
         else
         {
             movies = cached.Select(MovieCardModel.ToListCard);
         }
-        return movies.OrderBy(m=> m.Title).ThenBy(m=> m.Year);
+        return movies.OrderBy(m => m.Title).ThenBy(m => m.Year);
+    }
+
+    private void UpdateCache()
+    {
+        Task.Run(async () =>
+        {
+            IEnumerable<ListCard> movies = await Client.GetMovies();
+            await CacheContext.Movies.CleanAsync();
+            await CacheContext.Movies.AddAsync(movies.Select(MovieCardModel.FromListCard).ToList());
+            await JsRuntime.InvokeAsync<DateTime?>("localStorage.setItem", "last-update", DateTime.UtcNow);
+        });
     }
 
     private async ValueTask<bool> GetShouldUpdate()
     {
-        bool result;
+        bool result = await GetShouldDayUpdate(); 
         try
         {
-            result = await JsRuntime.InvokeAsync<bool>("localStorage.getItem", "CATALOG");
+            if (!result)
+                result = await JsRuntime.InvokeAsync<bool>("localStorage.getItem", "CATALOG");
         }
         catch
         {
-            result = false;
+            result = await GetShouldDayUpdate();
+        }
+        Console.WriteLine($"GetShouldUpdate {result}");
+        return result;
+    }
+
+    private async ValueTask<bool> GetShouldDayUpdate()
+    {
+        bool result;
+        try
+        {
+            DateTime? lastUpdate = await JsRuntime.InvokeAsync<DateTime?>("localStorage.getItem", "last-update");
+            if (lastUpdate.HasValue)
+            {
+                DateOnly last = DateOnly.FromDateTime(lastUpdate.Value);
+                DateOnly actual = DateOnly.FromDateTime(DateTime.Today);
+                result = last != actual;
+            }
+            else result = true;
+        }
+        catch
+        {
+            result = true;
         }
         return result;
+
     }
 
 }
