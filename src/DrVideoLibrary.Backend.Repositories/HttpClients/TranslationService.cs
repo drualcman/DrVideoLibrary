afee3d26118b2cdee3cc518ae46dcbbfeb94737b
Sergi Ortiz Gomez <drualcman@gmail.com>
2025-01-16 09:06:34 +0800

    Improves no need to translate when is not necesary or lang it's not correct sent.

--- src/DrVideoLibrary.Backend.Repositories/HttpClients/TranslationService.cs
+++ src/DrVideoLibrary.Backend.Repositories/HttpClients/TranslationService.cs
@@ -14,24 +14,53 @@ internal class TranslationService : ITranslateService
 
     public async Task<string> TranslateTextAsync(string text, string fromLanguage, string targetLanguage)
     {
-        Dictionary<string, string> requestBody = new Dictionary<string, string>
-        {
-            { "auth_key", Options.TranslationApiKey },
-            { "text", text },
-            { "source_lang", fromLanguage },
-            { "target_lang", targetLanguage }
-        };
-
-        FormUrlEncodedContent requestContent = new FormUrlEncodedContent(requestBody);
-        HttpResponseMessage response = await Client.PostAsync("translate", requestContent);
-        response.EnsureSuccessStatusCode();
-
-        string responseContent = await response.Content.ReadAsStringAsync();
-        TranslationResult translationResult = JsonSerializer.Deserialize<TranslationResult>(responseContent, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
         string result = text;
-        if (translationResult != null)
-            result = translationResult.Translations.FirstOrDefault()?.Text;
+        string from = GetTwoLetterLanguageCode(fromLanguage);
+        string to = GetTwoLetterLanguageCode(targetLanguage);
+        if (CanTransalte(from, to))
+        {
+            Dictionary<string, string> requestBody = new Dictionary<string, string>
+            {
+                { "auth_key", Options.TranslationApiKey },
+                { "text", text },
+                { "source_lang", from },
+                { "target_lang", to }
+            };
 
+            FormUrlEncodedContent requestContent = new FormUrlEncodedContent(requestBody);
+            HttpResponseMessage response = await Client.PostAsync("translate", requestContent);
+            if (response.IsSuccessStatusCode)
+            {
+                string responseContent = await response.Content.ReadAsStringAsync();
+                TranslationResult translationResult = JsonSerializer.Deserialize<TranslationResult>(responseContent, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+                if (translationResult != null)
+                    result = translationResult.Translations.FirstOrDefault()?.Text;
+            }
+            else
+            {
+                string error = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(error);
+            }
+        }
+        return result;
+    }
+
+    private string GetTwoLetterLanguageCode(string language)
+    {
+        string result = language;
+        if (!string.IsNullOrWhiteSpace(language))
+        {
+            result = language.Length >= 2 ? language.Substring(0, 2).ToLowerInvariant() : language.ToLowerInvariant();
+        }
+        return result;
+    }
+
+    private bool CanTransalte(string from, string target)
+    {
+        bool result;
+        if (string.IsNullOrEmpty(from) || string.IsNullOrEmpty(target)) result = false;
+        else if (from.Equals(target, StringComparison.OrdinalIgnoreCase)) result = false;
+        else result = true;
         return result;
     }
 
