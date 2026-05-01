e513dec890d0781adfbcb6a2fdba6d0dcc474693
Sergi Ortiz Gómez <drualcman@msn.com>
2026-05-02 07:54:50 +0800

    Add global using

--- src/DrVideoLibrary.Backend.Storage/GlobalUsings.cs
+++ src/DrVideoLibrary.Backend.Storage/GlobalUsings.cs
@@ -1,6 +1,7 @@
 global using Azure.Storage.Blobs;
 global using Azure.Storage.Blobs.Models;
 global using Azure.Storage.Sas;
+global using DrVideoLibrary.Backend.ApplicationBusinessRules.Interfaces;
 global using DrVideoLibrary.Backend.ApplicationBusinessRules.Options;
 global using DrVideoLibrary.Backend.Storage;
 global using DrVideoLibrary.Backend.Storage.Helpers;
